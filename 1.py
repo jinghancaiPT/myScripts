ee2f2e386972541469eb3800964ef31653815a02
jinghancaiPT <jinghan.cai@gmail.com>
2021-01-23 16:51:23 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 14:57:13 2019
+
+@author: 681168
+"""
+
+for i in range(0,20):
+    ii='H'+str(i)
+    print (round(int(ii[1:3])/2-0.4)+1)
+    
+    
